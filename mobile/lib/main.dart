2641aaf2041919dca9c3221580a942047a7b7edf
ckfrancois <francois.chelseak@gmail.com>
2026-04-07 17:53:47 -0400

    Login/Signup Aesthetics Done

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:convert';
 
 const Color black = Color.fromARGB(255, 14, 7, 2);
@@ -29,6 +30,9 @@ class MyApp extends StatelessWidget {
           headlineLarge: TextStyle(color: white, fontSize: 32, fontWeight: FontWeight.bold),
           bodyMedium: TextStyle(color: white, fontSize: 16),
           bodyLarge: TextStyle(color: white, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        textTheme: GoogleFonts.exoTextTheme(
+          Theme.of(context).textTheme,
         ),
         //colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
@@ -237,7 +241,7 @@ class _LoginPageState extends State<LoginPage> {
                         style: style,
                         child: _isLoading
                             ? const CircularProgressIndicator()
-                            : const Text('Login', style: TextStyle(fontSize: 16, color: white)),
+                            : const Text('Login', style: TextStyle(fontSize: 16, color: white, fontWeight: FontWeight.bold)),
                       ),
                       const SizedBox(height: 16),
                       if (_errorMessage.isNotEmpty)
@@ -300,7 +304,7 @@ class _SignupPageState extends State<SignupPage> {
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         if (data['error'] == null || data['error'].isEmpty) {
-          // Login successful, navigate to home page
+          // Signup successful, navigate to home page
           if (mounted) {
             Navigator.pushReplacement(
               context,
@@ -334,6 +338,7 @@ class _SignupPageState extends State<SignupPage> {
     }
   }
 
+/*
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style = ElevatedButton.styleFrom(
@@ -549,10 +554,301 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       );
+  }*/
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+      backgroundColor: green,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: 
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage()
+                )
+              );
+              }, 
+            icon: Image.asset("assets/images/domino.png"),
+            ),
+        title: const Text('DOMINOES',
+        style: TextStyle(color: white)),
+        backgroundColor: black,
+        ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/woodBG.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  color: black,
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: white),
+                    textAlign: TextAlign.center,
+                  ),
+                ), 
+                const Divider(
+                  height: 5,
+                  thickness: 5,
+                  color: green,
+                ),
+                Container (
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/WoodGrain.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),                  
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                      const SizedBox(height: 20),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child:TextButton( // remove const to allow for navigation
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context, 
+                            MaterialPageRoute(
+                              builder: (context) => const LoginPage()
+                              )
+                              );
+                        },
+                        child: 
+                            Text.rich(
+                              const TextSpan(
+                                text: "Already have an account? ",
+                                children: [
+                                  TextSpan(
+                                    text: "Sign In",
+                                    style: TextStyle(color: green, fontWeight: FontWeight.bold), 
+                                  ),
+                              ],
+                            ),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .color!
+                                      .withValues(alpha: 0.8),
+                                ),
+                          ),
+                      ),
+                      ),
+                      
+                      const SizedBox(height:16),
+                      TextField(
+                        controller: _loginController,
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _emailController,
+                        //obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _firstNameController,
+                        //obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'First Name',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _lastNameController,
+                        //obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Last Name',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      ElevatedButton(
+                        onPressed: _isLoading ? null : _login,
+                        style: style,
+                        child: _isLoading
+                            ? const CircularProgressIndicator()
+                            : const Text('Sign Up', style: TextStyle(fontSize: 16, color: white, fontWeight: FontWeight.bold)),
+                      ),
+                      const SizedBox(height: 16),
+                      if (_errorMessage.isNotEmpty)
+                        Text(
+                          _errorMessage,
+                          style: const TextStyle(color: Colors.red, fontSize: 14),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   void dispose() {
     super.dispose();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _loginController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  //String _errorMessage = '';
+  //bool _isLoading = false;
+  Future<void> _login() async {
+    setState(() {
+      //_isLoading = true;
+      //_errorMessage = '';
+    });
+    /*try {
+      final response = await http.post(
+        Uri.parse('http://rickymetral.xyz:5000/api/signup'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'login': _loginController.text,
+          'firstName': _firstNameController.text,
+          'lastName': _lastNameController.text,
+          'email': _emailController.text,
+          'password': _passwordController.text,
+        }),
+      );
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        if (data['error'] == null || data['error'].isEmpty) {
+          // Login successful, navigate to home page
+          /*if (mounted) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePage(
+                  userId: data['id'],
+                  firstName: data['firstName'],
+                  lastName: data['lastName'],
+                ),
+              ),
+            );
+          }
+        } else {
+          setState(() {
+            _errorMessage = data['error'];
+          });*/
+        }
+      } else {
+        setState(() {
+          //_errorMessage = 'Signup failed. Please try again.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        //_errorMessage = 'Network error. Please check your connection.';
+      });
+    } finally {
+      setState(() {
+        //_isLoading = false;
+      });
+    }*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DOMINOES'),
+        ),
+        body:Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Home Page',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ]
+            ),
+          ),
+        ),
+      );
   }
 }
