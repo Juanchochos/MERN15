2bcb4727a149747a1d600182de05d816acdd7a40
James Phillips <jamesphillips052404@gmail.com>
2026-04-07 11:10:27 -0400

    added temporary signup function

---
+++ backend/seed.cjs
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const md5 = require('md5');
+const mongoose = require("mongoose");
+const User = require("./models/user.cjs");
+
+async function seedUser() {
+  try {
+    // Connect to MongoDB Atlas
+    const url = process.env.MONGODB_URI;
+    await mongoose.connect(url);
+    console.log("MongoDB connected");
+
+    const hashedPassword = await md5('password');
+
+    await User.create({
+      login: 'james',
+      firstName: 'James',
+      lastName: 'Phillips',
+      email: 'james@gmail.com',
+      password: hashedPassword,
+    });
+
+    console.log('User created');
+    await mongoose.connection.close();
+    process.exit(0);
+  } catch (error) {
+    console.error('Error creating user:', error.message);
+    process.exit(1);
+  }
+}
+
+seedUser();
