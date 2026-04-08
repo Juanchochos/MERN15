f39d9df796767eb515b838edf1fd349684a0b935
William Douglass <w.douglass1127@gmail.com>
2026-04-07 23:18:50 -0400

    Game Board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import CardPage from './pages/CardPage';
 import CreatePage from './pages/CreatePage';
+import GameRoomPage from './pages/GameRoomPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
 		<Route path="/signup" element={<SignUpPage />} />
 		<Route path="/create" element={<CreatePage />} />
         <Route path="/cards" element={<CardPage/>}/>
+        <Route path="/game" element={<GameRoomPage/>} />
         <Route path="*" element={<Navigate to="/" replace />}/>
       </Routes>  
     </Router>
