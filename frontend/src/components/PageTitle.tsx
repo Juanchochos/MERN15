66274620e111d7b0ca953abc7a2d241c02096ff1
luke-vernon <lukevernon12345@gmail.com>
2026-04-14 17:30:41 -0400

    Added MatchHistory Page and fixed domino icon scaling issues

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -4,7 +4,7 @@ function PageTitle()
    <header role = "banner">
    <div className = "containerHeader">
 	<div className="titleWrapper">
-	 <img src="/img/domino.png" id="dominoIcon" alt="Domino Icon"></img>
+	 <img src="/img/domino.png" id="dominoIconSmall" alt="Domino Icon"></img>
      <h1 id="title">Dominoes</h1>
 	 </div>
 	</div>
